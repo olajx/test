dff000f3ec769e2bb2f8faa807bac0b886bc1613
olajx <perfanni@gmail.com>
2016-11-01 13:56:39 +0100

    Update test.py

--- test.py
+++ test.py
@@ -1,25 +1,29 @@
 import unittest
 
+
 # Here's our "unit".
 
-
 def IsOdd(n):
-    return
+    if n % 2 == 0: 
+         return False
+    else:
+         return True
+         
 
 # Here's our "unit tests".
 
-
 class IsOddTests(unittest.TestCase):
-
+    
     def testOne(self):
         self.failUnless(IsOdd(1))
-
+    
     def testTwo(self):
         self.failIf(IsOdd(2))
 
-
 def main():
     unittest.main()
-
 if __name__ == '__main__':
     main()
+
+
+    
